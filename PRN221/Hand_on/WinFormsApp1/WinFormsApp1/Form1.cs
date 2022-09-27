a8485247cc567a3a59fb5bebfca8c318669365f3
Denkhotieu <denkhotieu@gmail.com>
2022-09-27 15:52:52 +0700

    updated

---
+++ PRN221/Hand_on/WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class frm1 : Form
+    {
+        public frm1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtNhapLieu.Text = "";
+            
+        }
+
+        private void hienthi_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtNhapLieu.Text = "Hello World!!";
+          
+        }
+
+        private void View_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void txtNhapLieu_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            hienthi.Text = mutiText.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if(mutiText.Text.Trim() == "")
+            {
+                button4.Enabled = false;
+            }
+            else
+            {
+                button4.Enabled = true;
+            }
+        }
+    }
+}
